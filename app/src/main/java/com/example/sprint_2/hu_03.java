abd6358da2c2cf8cfbc57db0e485c5aa689e4d2b
CondeMike <quichinguito@gmail.com>
2021-11-22 23:19:45 +0200

    primera implementacion h06

--- app/src/main/java/com/example/sprint_2/hu_03.java
+++ app/src/main/java/com/example/sprint_2/hu_03.java
@@ -5,16 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class hu_03 extends AppCompatActivity {
-
+public class hu_03 extends AppCompatActivity{
     private ImageView regresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hu_03);
+
 
         regresar=(ImageView)findViewById(R.id.regresar_Menu);
         regresar.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +27,25 @@ public class hu_03 extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+
+// start implementation - Pendiente modificación de la instancia de la clase...
+
+        EditText txtLabelSearch = (EditText) findViewById(R.id.txtInputSearch3);
+        Button btSearch;
+        btSearch = (Button) findViewById(R.id.Bt_buscar);
+        btSearch.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                ValidacionCampos vc= new ValidacionCampos();
+                boolean isValid= vc.formEtiqueta(txtLabelSearch.getText().toString());
+                if (!isValid){
+                    Toast.makeText(hu_03.this, "El campo NO debe estar vacio, debe tener como minimo dos letras y no puede contener caracteres especiales",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+// end implementation.
+
     }
 }
